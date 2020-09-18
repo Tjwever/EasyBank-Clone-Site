6ba7e0bb3a4051f2509b2a8ef71755028beeca10
Timothy Wever <Timjwever@gmail.com>
2020-09-17 21:40:30 -0400

    Last thing

---
+++ app.js
@@ -0,0 +1,5 @@
+function menuFunc(){
+    document.querySelector(".menu-btn").classList.toggle("change");
+    document.querySelector(".nav-bar-mobile").classList.toggle("change");
+    document.getElementById("nav-gradient").classList.toggle("change");
+}
